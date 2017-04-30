6f9473f6ae42a7097c295ad7e9918eb53031d499
Tanner Summers <tannersummers@gmail.com>
2017-04-30 12:27:39 -0700

    before merge

--- data_util.py
+++ data_util.py
@@ -5,112 +5,194 @@ import json
 
 
 class NumpyException(Exception):
-	pass
+    pass
 
 
-class DotaData:
-	def __init__(self):
-		self.base_api = "https://api.opendota.com/api/"
-
-	# load match ids from a chosen json file
-	def loadMatches(self, file_path):
-		# Load in json file of matches
-		with open(file_path) as data_file:
-			# save json to memory
-			return json.load(data_file)
-
-	# get json from api with parameter
-	def get(self, api):
-		# get request from api
-		# https://pyformat.info/
-		r = requests.get("{}{}".format(self.base_api, api))
-		# make sure request has good status code
-		assert r.status_code == 200
-		# return json of request object
-		return r.json()
+class SubDictException(Exception):
+    pass
 
 
-	def extract_base_features(self, data):
-		# get keys of the first data element as the form of a set
-		base_feature_set = set(data[0].keys())
-		# create an empty set for the extra features that do not belong to all the data (matches)
-		extras = set()
-		# loop all data
-		for d in data:
-			# create temp set using the keys from current match
-			f = set(d.keys())
-			# symmetric_difference: new set with elements in either s or t but not both
-			# https://docs.python.org/2/library/sets.html
-			# basically, get the features that do not belong to the base feature set
-			# and add them to the current set of other features that do not belong to the base feature set
-			extras = extras.union(base_feature_set.symmetric_difference(f))
-			# intersection: new set with elements common to s and t
-			# set base features of what features bwlong to both the base feature set and the temp f set
-			base_feature_set = base_feature_set.intersection(f)
-		# return the common features and the features that are unique
-		return base_feature_set, extras
+class DotaData(object):
+    def __init__(self):
+        self.base_api = "https://api.opendota.com/api/"
 
-	def np_ize(self, data):
-		# check if data is a list
-		if isinstance(data, list):
-			# use any element to get the keys (all elements may or may not have same features)
-			features = data[0].keys()
-			# Return True if all elements of the iterable are true (or if the iterable is empty).
-			# https://docs.python.org/2/library/functions.html#all
-			# checks if all keys of the element are equal for all the other data elements
-			if all([d.keys() == features for d in data]):
-				# sort items: sorted(d.items()) for each d in data
-				# for k, v in sorted(d.items()): get key and vlaue for each sorted d
-				# get v as the value to put into list
-				# we use k, v so seperate the key ans vlaue or else it would display both
-				# now we have list of features (values)
-				l = [[v for k, v in sorted(d.items())] for d in data]
-				# return the feautres (keys) and a np array (vector) of the list of features (values)
-				return features, np.array(l)
-			# there are features that do no belong to all api calls (all data / all matches)
-			else:
-				# get the common features and unique features
-				base_feature_set, extra_features = self.extract_base_features(data)
-				# loop the data : d
-				# get all values in d
-				# if that value belongs to the base_feature_set
-				# add to list
-				l = [[v for k, v in sorted(d.items()) if k in base_feature_set] for d in data]
-				# return the common features and a np vector of the data of that feature set
-				return base_feature_set, np.array(l)
-		raise NumpyException("Unable to transform data into numpy array")
+    def get(self, api):
+        '''
+        Uses the requests module to get data from the api 
+        Returns the python object that corresponds to the api's json 
+        '''
+        r = requests.get("{}{}".format(self.base_api, api))
+        assert r.status_code == 200
+        return r.json()
+
+    def extract_base_features(self, data):
+        '''
+        Extracts keys (features) from a list of dicts 
+        Returns a set of features that all dicts contain, 
+            as well as a set of all the extra keys 
+        (extra keys are supplied to show how messed up opendota's api is, 
+            only features in the base_feature_set should be used)
+        '''
+        base_feature_set = set(data[0].keys())
+        extras = set()
+        for d in data:
+            f = set(d.keys())
+            extras = extras.union(base_feature_set.symmetric_difference(f))
+            base_feature_set = base_feature_set.intersection(f)
+
+        return base_feature_set, extras
+
+    def np_ize(self, data, np_only=False):
+        '''
+        Turns a list of dicts into an np array 
+        Returns only the subset of keys that belong to all dicts in the list, plus a numpy array
+        '''
+        if np_only is True:
+            return np.array(data)
+        if isinstance(data, list):
+            features = data[0].keys()
+            if all([d.keys() == features for d in data]):
+                l = [[v for k, v in sorted(d.items())] for d in data]
+                return features, np.array(l)
+            else:
+                base_feature_set, extra_features = self.extract_base_features(data)
+                l = [[v for k, v in sorted(d.items()) if k in base_feature_set] for d in data]
+                return base_feature_set, np.array(l)
+        raise NumpyException("Unable to transform data into numpy array")
+
+    def sub_dicts(self, data, desired_keys):
+        '''
+        Transforms dictionaries into sub dictionaries
+        Returns a list of dictionaries comprised of the desired_keys
+        '''
+        base_feature_set, extras = self.extract_base_features(data)
+        if all(k in base_feature_set for k in desired_keys):
+            return [{k: d[k] for k in desired_keys} for d in data]
+        raise SubDictException("Unable to extract sub dict. Not all keys exist in every member of the data set.")
+
+    def read_json_file(self, filepath):
+        '''
+        Reads a json file 
+        Returns a python object that corresponds to the file's json
+        '''
+        with open(filepath, 'r') as f:
+            return json.load(f)
+
+    def write_json_file(self, filepath, data):
+        '''
+        Writes a json file; writes json of python object
+        '''
+        with open(filepath, 'w') as f:
+            json.dump(data, f, indent=4)
+
+    def shorten_data(self, data, desired_keys):
+        '''
+        Takes in data as a list of dicts, and desired_keys, which mimics the structure of the dicts to be returned
+        Returns a list of dicts with the structure of desired_keys (2 levels only)
+        '''
+        assert isinstance(desired_keys, dict)
+        _data = self.sub_dicts(data, desired_keys.keys())
+        for d in _data:
+            for key, value in desired_keys.iteritems():
+                if value is not None:
+                    assert isinstance(value, list)
+                    assert isinstance(d[key], list)
+                    d[key] = self.sub_dicts(d[key], value)
+        return _data
+
+
+class BasicHeroData(DotaData):
+    '''
+    Basic Usage: Get dota data however you can, be it loaded from a file or directly from the api
+        then call load_data on it
+    '''
+
+    def __init__(self):
+        super(BasicHeroData, self).__init__()
+        self.hero_features, self.hero_id_index_map = self.heroes()
+        self.target_labels = ['radiant_win', 'dire_win']
+
+    def heroes(self):
+        heroes = self.read_json_file('./Data/heroes.json')['heroes']
+        id_name_map = {h['id']: h['name'] for h in heroes}
+        ids = id_name_map.keys()
+        id_index_map = {x: i for i, x in enumerate(ids)}
+        hero_features = [''] * (2 * (len(id_index_map)))
+        for i, id in enumerate(id_index_map):
+            hero_features[i] = '{}_{}'.format(id_name_map[id], 'radiant')
+            hero_features[i + len(id_index_map)] = '{}_{}'.format(id_name_map[id], 'dire')
+        return hero_features, id_index_map
+
+    def process_matches(self):
+        targets = []
+        data = []
+        for match in self.shortened_data:
+            datum = [0] * (2 * len(self.hero_id_index_map))
+            for player in match['players']:
+                try:
+                    if player['isRadiant'] is True:
+                        index = self.hero_id_index_map[player['hero_id']]
+                    else:
+                        index = self.hero_id_index_map[player['hero_id']] + len(self.hero_id_index_map)
+                    datum[index] = 1
+
+                except KeyError:  # there is some data that has hero_ids that aren't in the heroes.json
+                    datum = [0] * len(datum)
+                    break
+            if any([x != 0 for x in datum]):
+                data.append(datum)
+                targets.append([int(match['radiant_win']), int(not match['radiant_win'])])
+        return data, targets
+
+    def load_data(self, matches):
+        self.shortened_data = self.shorten_data(matches, {'players': ['isRadiant', 'hero_id'], 'radiant_win': None})
+        data, targets = self.process_matches()
+        self.raw_data = data
+        self.data = self.np_ize(data, True)
+        self.targets = self.np_ize(targets, True)
 
 
 if __name__ == "__main__":
-	'''example usage'''
-	d = DotaData()
-	# data = d.get('publicMatches')
-	data = d.loadMatches('./Data/Matches_By_Id/10_matches.json')
-	features, _data = d.np_ize(data)
-	matches = []
-	match_ids = [datum['match_id'] for datum in data]
-	for mid in match_ids[:5]:# just doing 2 in this example so it doesn't take too long
-	# for mid in match_ids:
-		matches.append(d.get("matches/{}".format(mid)))
-		sleep(1) # the opendota api requests that this endpoint only be hit 1/s
+    h = BasicHeroData()
 
 
-	print(matches)
+    matches_by_id = h.read_json_file('./Data/Matches_By_Id/10_matches.json')
+    # features, _data = h.np_ize(matches_by_id)
+    matches = []
+    match_ids = [datum['match_id'] for datum in matches_by_id]
+    for mid in match_ids:
+        # for mid in match_ids:
+        matches.append(h.get("matches/{}".format(mid)))
+        sleep(1.5)  # the opendota api requests that this endpoint only be hit 1/s
+    h.write_json_file('./Data/Matches/10_matches_full.json', matches)
 
 
-	#
-	# features, _data = d.np_ize(matches)
+
+
+
+    # h.load_data(matches)
     #
-    # print(features)
-	print()
-	print()
-	print()
-	print()
-	print()
-	# print(_data)
+    # features = h.hero_features
+    # data = h.data
+    # target_labels = h.target_labels
+    # targets = h.targets
+    #
+    # assert len(data[0]) == len(features)
+    # assert len(targets[0]) == len(target_labels)
+    #
+    # h.write_json_file('./Data/Matches/5_matches_short.json', h.shortened_data)
 
 
-
-
-
-
+    # h = BasicHeroData()
+    # matches = h.read_json_file('./Data/Matches/5_matches.json')
+    # h.load_data(matches)
+    #
+    # features = h.hero_features
+    # data = h.data
+    # target_labels = h.target_labels
+    # targets = h.targets
+    #
+    # assert len(data[0]) == len(features)
+    # assert len(targets[0]) == len(target_labels)
+    #
+    # h.write_json_file('./Data/Matches/5_matches_short.json', h.shortened_data)
