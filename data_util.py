70268a3038b14fed6d2e35568bef8faaf9b41433
elinor <eahuntington@gmail.com>
2017-04-29 17:09:14 -0700

    Added comments to DotaData

--- data_util.py
+++ data_util.py
@@ -15,11 +15,22 @@ class DotaData:
 		self.base_api = "https://api.opendota.com/api/"
 
 	def get(self, api):
+		'''
+		Uses the requests module to get data from the api 
+		Returns the python object that corresponds to the api's json 
+		'''
 		 r = requests.get("{}{}".format(self.base_api, api))
 		 assert r.status_code == 200
 		 return r.json()
 
 	def extract_base_features(self, data):
+		'''
+		Extracts keys (features) from a list of dicts 
+		Returns a set of features that all dicts contain, 
+			as well as a set of all the extra keys 
+		(extra keys are supplied to show how messed up opendota's api is, 
+			only features in the base_feature_set should be used)
+		'''
 		base_feature_set = set(data[0].keys())
 		extras = set()
 		for d in data:
@@ -30,6 +41,10 @@ class DotaData:
 		return base_feature_set, extras	
 
 	def np_ize(self, data):
+		'''
+		Turns a list of dicts into an np array 
+		Returns only the subset of keys that belong to all dicts in the list, plus a numpy array
+		'''
 		if isinstance(data, list):
 			features = data[0].keys()
 			if all([d.keys() == features for d in data]):
@@ -42,12 +57,20 @@ class DotaData:
 		raise NumpyException("Unable to transform data into numpy array")
 
 	def sub_dicts(self, data, desired_keys):
+		'''
+		Transforms dictionaries into sub dictionaries
+		Returns a list of dictionaries comprised of the desired_keys
+		'''
 		base_feature_set, extras = self.extract_base_features(data)
 		if all(k in base_feature_set for k in desired_keys):
 			return [{k:d[k] for k in desired_keys} for d in data]
 		raise SubDictException("Unable to extract sub dict. Not all keys exist in every member of the data set.")
 
 	def read_json_file(self, filepath):
+		'''
+		Reads a json file 
+		Returns a python object that corresponds to the file's json
+		'''
 		with open(filepath, 'r') as f:
 			return json.load(f)
 
