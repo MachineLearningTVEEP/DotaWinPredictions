a87eb789060f4a8ff90a5b76e862077b606b03ca
Tanner Summers <tannersummers@gmail.com>
2017-05-07 13:39:56 -0700

    Revert "merging"
    This reverts commit d68c0d47c974213131026303dd26e2c6d02dae69, reversing
    changes made to 6f9473f6ae42a7097c295ad7e9918eb53031d499.

--- test_data_util.py
+++ test_data_util.py
@@ -1,9 +1,5 @@
 import unittest
-import json
 from data_util import DotaData
-
-def p(obj):
-    print json.dumps(obj, indent=4)
 
 class TestDotaData(unittest.TestCase):
     def setUp(self):
@@ -25,46 +21,6 @@ class TestDotaData(unittest.TestCase):
         features, extras = self.dota_data.extract_base_features(data)
         self.assertEqual(features, set([1]))
         self.assertEqual(extras, set([2,3,4,5,6,7]))
-
-    def test_shorten_dict(self):
-        desired = {
-            'b': None,
-            'c': ['d', 'e', 'f'],
-            'g': None,
-            'h': None
-        }
-        actual_d = {
-            'b': 0,
-            'c': [{
-                'd': 0,
-                'e': 0,
-                'f': 0,
-                'a': []
-            }],
-            'g': 0,
-            'h': 0,
-            'j': {},
-            'k': []
-        }
-        actual_l = [actual_d]
-        p(actual_l)
-        data = self.dota_data.shorten_data(actual_l, desired)
-        p(data)
-
-        for d in data:
-            self.assertEqual(len(d), len(desired))
-            self.assertEqual(d.keys(), desired.keys())
-            for k,v in d.iteritems():
-                if isinstance(v, list):
-                    for _d in v:
-                        self.assertEqual(len(_d), len(desired[k]))
-                        self.assertEqual(sorted(_d.keys()), sorted(desired[k]))
-                        for _k, _v in _d.iteritems():
-                            self.assertEqual(_v, actual_d[k][0][_k])
-                else:
-                    self.assertEqual(v, actual_d[k])
-
-
 
 if __name__ == '__main__':
     unittest.main()
